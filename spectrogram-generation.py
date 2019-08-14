908b73cf10901dfabb29bb88e54197b1419d8c3d
Devang J <devang.j1998@gmail.com>
2019-08-14 13:54:23 +0530

    made spectrogram transparent

--- spectrogram-generation.py
+++ spectrogram-generation.py
@@ -39,5 +39,5 @@ for genre in genres:
         y, sr = librosa.load(songname, mono=True, duration=5)
         plt.specgram(y, NFFT=2048, Fs=2, Fc=0, noverlap=128, cmap=cmap, sides='default', mode='default', scale='dB');
         plt.axis('off');
-        plt.savefig(f'{genre_img_path}/{filename[:-4]}.png')
+        plt.savefig(f'{genre_img_path}/{filename[:-4]}.png', transparent=True)
         plt.clf()
