8232b68e77303de8b313b5979fdb96060e078fe4
Devang J <devang.j1998@gmail.com>
2019-09-21 11:24:35 +0530

    final data creation commit

--- song-generation.py
+++ song-generation.py
@@ -5,6 +5,7 @@ import os
 import pydub
 import base64
 import slugify
+
 # Create your own config.py containing client ID and secret
 from config import CLIENT_ID, CLIENT_SECRET
 
@@ -28,6 +29,7 @@ genres = {
     "rock"
 }
 
+# Size of dataset for each genre
 dataset_size = 100
 
 
@@ -35,31 +37,40 @@ dataset_size = 100
 # Note: won't give 100 recommendations since some songs don't have preview urls. It is advised to run this a few times so that new suggestions get added
 # Note 2: There are many genres in the list by spotify, it would be a better idea to choose a few genres which are popular and varying and work on those rather than risk underfitting the data
 
-try:
-    os.stat('data')
-except:
-    os.mkdir('data') 
-
-for genre in genres:
-    print(genre)
-    genre_path = f'data/{genre}'
+def get_song_recommendations():
     try:
-        os.stat(genre_path)
+        os.stat('data')
     except:
-        os.mkdir(genre_path)
-    while len(os.listdir(genre_path)) < dataset_size:
-        print("getting recommendations")
-        rec = sp.recommendations(seed_genres=[genre], limit = dataset_size)
+        os.mkdir('data') 
+
+    for genre in genres:
+        print(genre)
+        genre_path = f'data/{genre}'
+        try:
+            os.stat(genre_path)
+        except:
+            os.mkdir(genre_path)
         song_list = []
-        for track in rec['tracks']:
-            track_name = track['name']
-            track_url = track['preview_url']
-            if track_url and track_name not in song_list:
-                print(f'\tSong Name: {track_name}')
-                song_list.append(track_name)
-                r = requests.get(track_url)
-                print("\tmaking mp3")
-                track_name = slugify.slugify(track_name)
-                print(f'\tFile Name: {track_name}\n')
-                with open(f'{genre_path}/{track_name}.mp3','wb') as f:
-                    f.write(r.content)
+        while len(os.listdir(genre_path)) < dataset_size:
+            print("Getting recommendations")
+            # for metal use two sub genres explicitly to get more than 34 songs
+            rec = sp.recommendations(seed_genres=[genre], limit = dataset_size)
+            if rec['tracks']:
+                for track in rec['tracks']:
+                    track_name = track['name']
+                    track_url = track['preview_url']
+                    if track_url and track_name not in song_list:
+                        print(f'\tSong Name: {track_name}')
+                        song_list.append(track_name)
+                        r = requests.get(track_url)
+                        print("\tmaking mp3")
+                        track_name = slugify.slugify(track_name)
+                        print(f'\tFile Name: {track_name}\n')
+                        with open(f'{genre_path}/{track_name}.mp3','wb') as f:
+                            f.write(r.content)
+                            print(f'\tFile Made {len(song_list)}\n')
+                            f.close()
+
+
+# Calling the function
+get_song_recommendations()
