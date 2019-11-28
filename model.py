dd69fe77befa0a37d5f65e9ffae4cb85625629d9
Devang J <devang.j1998@gmail.com>
2019-11-28 13:30:43 +0530

    Update model.py

--- model.py
+++ model.py
@@ -22,10 +22,10 @@ def song_to_img(song):
     if song.endswith('.mp3'):
         sound = pydub.AudioSegment.from_mp3(f'songs/{song}')
         song = song[-4] + '.wav'
-        sound.export(f'{app.config['UPLOAD_FOLDER']}{song}', format="wav")
-    y, sr = librosa.load(f'{app.config['UPLOAD_FOLDER']}{song}', mono=True, duration=5)
+        sound.export(f'songs/{song}', format="wav")
+    y, sr = librosa.load(f'songs/{song}', mono=True, duration=5)
     plt.specgram(y, NFFT=2048, Fs=2, Fc=0, noverlap=128, cmap=cmap, sides='default', mode='default', scale='dB')
     plt.axis('off')
-    plt.savefig(f'{app.config['UPLOAD_FOLDER']}{song[:-4]}.png', transparent=True)
+    plt.savefig(f'songs/{song[:-4]}.png', transparent=True)
     plt.clf()
     
